3459a69c6463f96929ff3802938571c25eaf07a5
mburrows18 <mburrows@bu.edu>
2016-12-28 17:16:46 -0500

    does NOT get rid of duplicate bonds but DOES generate correct bond angles in degrees

--- connectivitymap.py
+++ connectivitymap.py
@@ -1,5 +1,6 @@
-from numpy import sqrt, power
+from numpy import sqrt, power, linalg
 import numpy as np
+
 
 f1 = open('EPO_optimized.pdb', 'r')
 f2 = open('epo.itp', 'r')
@@ -9,10 +10,8 @@ pdbList = f1.read().splitlines()
 
 #coordinates
 coordinates = []
-#connectivity by atom name without permutative duplicates
+#connectivity by atom name
 connectivity = []
-#connectivity by atom name with duplicates
-allconnectivity = []
 #connectivity by atom index number
 connectivitymap = []
 #distance threshold
@@ -25,7 +24,7 @@ aname = []
 bdist = []
 #bonds in the form [atom, coordinates],  [[bonded atom, coordinates], [bonded atom, coordinates]], etc
 bondcoords = []
-#coord for angles in the form [xyz atom], [xyz central atom], [xyz atom]
+#coord for angles in the form [xyz atom], [xyz central atom] [xyz atom]
 anglecoords = []
 #bonds in the form atom, [bonded atoms]
 bonds = []
@@ -59,7 +58,6 @@ for line in pdbList:
 for i in range(len(coordinates)):
     atom_coord_x, atom_coord_y, atom_coord_z = coordinates[i][1][0], coordinates[i][1][1], coordinates[i][1][2]
     connectivity.append([aname[i], []])
-    allconnectivity.append([aname[i], []])
     connectivitymap.append([i + 1, []])
     bdist.append([aname[i], [], []])
     atomcoords.append((aname[i], (atom_coord_x, atom_coord_y,atom_coord_z)))
@@ -69,19 +67,22 @@ for i in range(len(coordinates)):
             power((target_coord_y - atom_coord_y), 2) + 
             power((target_coord_z - atom_coord_z), 2))
         if (distance < dist_thresh) and (i != j):
-            allconnectivity[i][1].append(aname[j])
-        if (distance < dist_thresh) and (i != j) and (i < j):
             connectivity[i][1].append(aname[j])
             connectivitymap[i][1].append(j + 1)
             bdist[i][1].append(j + 1)
             bdist[i][2].append(str(distance))
-            bondcoords.append([[aname[i], atomcoords[i][1]], []])
-            bonds.append([aname[i], []])
+#print ("\n", file = f3)
 print ("Bonding in EPO:", file = f3)
 for i in range (len(connectivity)):
-    if len(connectivity[i][1]) !=0:
-        print (connectivity[i], file = f3)
+    #if len(connectivity[i][1]) == 1:
+        #print ('{} is bonded to:'.format(aname[i]), file = f3)
+        #print ('        {} \n with bond length {}'.format(', '.join(connectivity[i][1]), ', '.join(bdist[i][2])), file = f3)
+        #print ('{} makes one bond'.format(aname[i]))
     if len(connectivity[i][1]) != 1:
+        #print ('{} makes multiple bonds'.format(aname[i]))
+        #print ('{} with coordinates {} is bonded to:'.format(aname[i], atomcoords[i][1]), file = f3)
+        bondcoords.append([[aname[i], atomcoords[i][1]], []])
+        bonds.append([aname[i], []])
         for j in range (len(connectivity[i][1])):
             #lines 76-84 are used to determine index of atom bonded to aname[i] in order to produce corresponding xyz coordinates
             Aname = connectivity[i][1][j]
@@ -93,96 +94,134 @@ for i in range (len(connectivity)):
                 Aindex = (int(Aname.strip('N')) - 1)
             if Aname.startswith("O"):
                 Aindex = (int(Aname.strip('O')) - 1)
+            #print ('        {} with coordinates {}'.format(connectivity[i][1][j], atomcoords[Aindex][1]), file = f3)
+            #print ('            {}-{} bond length = {} Angstroms'.format(aname[i], Aname, bdist[i][2][j]), file = f3)
             bondlengths.append((aname[i], Aname, bdist[i][2][j]))
             bondcoords[i][1].append([connectivity[i][1][j], atomcoords[Aindex][1]])
             #bondcoords[i][0] generates atom name with coordinates bondcoords[i][0][1]
             #bondcoords[i][1] generates [atom name, [coordinates]] of all bonded atoms
             bonds[i][1].append(connectivity[i][1][j])
+   
 for i in range (len(bonds)):
-    if len(bondcoords[i][1]) !=0:
-        atomb = np.array(bondcoords[i][0][1])
-        atoma = np.array(bondcoords[i][1][0][1])
-        atomc = np.array(bondcoords[i][1][1][1])  
-        F = atoma - atomb
-        E = atomb - atomc
-        angle = np.dot(F, E)
-        bondangle.append([[bondcoords[i][1][0][0], bondcoords[i][0][0], bondcoords[i][1][1][0]], []])
+    print ('atom {} makes {} bonds \n    and is bonded to {}'.format(aname[i], len(connectivity[i][1]), ', '.join(bonds[i][1])), file = f3)   
+    #print ('{} is bonded to {}'.format(bonds[i][0], ', '.join(bonds[i][1])), file = f3)
+for i in range (len(bondcoords)):
+    atomb = np.array(bondcoords[i][0][1])
+    atoma = np.array(bondcoords[i][1][0][1])
+    atomc = np.array(bondcoords[i][1][1][1])  
+    F = atoma - atomb
+    E = atomb - atomc
+    dot = np.dot(F, E)
+    normF = np.linalg.norm(F)
+    normE = np.linalg.norm(E)
+    cosangle = dot/(normE * normF)
+    angle = 180 - np.degrees(np.arccos(cosangle))
+    bondangle.append([[bondcoords[i][1][0][0], bondcoords[i][0][0], bondcoords[i][1][1][0]], []])
+    anglenumber +=1
+    numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][0][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][1][0])])])
+    bondangle[anglenumber - 1][1].append(str(angle))
+    if len(bondcoords[i][1]) == 3:
+        atomb1 = np.array(bondcoords[i][0][1])
+        atoma1 = np.array(bondcoords[i][1][1][1])
+        atomc1 = np.array(bondcoords[i][1][2][1])
+        F1 = atoma1 - atomb1
+        E1 = atomb1 - atomc1
+        dot1 = np.dot(F1, E1)
+        normF1 = np.linalg.norm(F1)
+        normE1 = np.linalg.norm(E1)
+        cosangle1 = dot1/(normE1 * normF1)
+        angle1 = 180 - np.degrees(np.arccos(cosangle1))
+        bondangle.append([[bondcoords[i][1][1][0], bondcoords[i][0][0], bondcoords[i][1][2][0]], []])
         anglenumber +=1
-        numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][0][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][1][0])])])
-        bondangle[anglenumber - 1][1].append(str(np.degrees(np.cos(angle))))
-        if len(bondcoords[i][1]) == 3:
-            atomb1 = np.array(bondcoords[i][0][1])
-            atoma1 = np.array(bondcoords[i][1][1][1])
-            atomc1 = np.array(bondcoords[i][1][2][1])
-            F1 = atoma1 - atomb1
-            E1 = atomb1 - atomc1
-            angle1 = np.dot(F1, E1)
-            bondangle.append([[bondcoords[i][1][1][0], bondcoords[i][0][0], bondcoords[i][1][2][0]], []])
-            anglenumber +=1
-            numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][1][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][2][0])])])
-            bondangle[anglenumber - 1][1].append(str(np.degrees(np.cos(angle1))))
-            atomb2 = np.array(bondcoords[i][0][1])
-            atoma2 = np.array(bondcoords[i][1][0][1])
-            atomc2 = np.array(bondcoords[i][1][2][1])
-            F2 = atoma2 - atomb2
-            E2 = atomb2 - atomc2
-            angle2 = np.dot(F2, E2)
-            bondangle.append([[bondcoords[i][1][0][0], bondcoords[i][0][0], bondcoords[i][1][2][0]], []])
-            anglenumber +=1
-            numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][0][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][2][0])])])
-            bondangle[anglenumber - 1][1].append(str(np.degrees(np.cos(angle2))))
-        if len(bondcoords[i][1]) == 4:
-            atomb1 = np.array(bondcoords[i][0][1])
-            atoma1 = np.array(bondcoords[i][1][1][1])
-            atomc1 = np.array(bondcoords[i][1][2][1])
-            F1 = atoma1 - atomb1
-            E1 = atomb1 - atomc1
-            angle1 = np.dot(F1, E1)
-            bondangle.append([[bondcoords[i][1][1][0], bondcoords[i][0][0], bondcoords[i][1][2][0]], []])
-            anglenumber +=1
-            numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][1][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][2][0])])])                
-            bondangle[anglenumber - 1][1].append(str(np.degrees(np.cos(angle1))))
-            atomb2 = np.array(bondcoords[i][0][1])
-            atoma2 = np.array(bondcoords[i][1][0][1])
-            atomc2 = np.array(bondcoords[i][1][2][1])
-            F2 = atoma2 - atomb2
-            E2 = atomb2 - atomc2
-            angle2 = np.dot(F2, E2)
-            bondangle.append([[bondcoords[i][1][0][0], bondcoords[i][0][0], bondcoords[i][1][2][0]], []])
-            anglenumber +=1
-            numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][0][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][2][0])])])
-            bondangle[anglenumber - 1][1].append(str(np.degrees(np.cos(angle2))))
-            atomb3 = np.array(bondcoords[i][0][1])
-            atoma3 = np.array(bondcoords[i][1][0][1])
-            atomc3 = np.array(bondcoords[i][1][3][1])
-            F3 = atoma3 - atomb3
-            E3 = atomb3 - atomc3
-            angle3 = np.dot(F3, E3)
-            bondangle.append([[bondcoords[i][1][0][0], bondcoords[i][0][0], bondcoords[i][1][3][0]], []])
-            anglenumber +=1
-            numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][0][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][3][0])])])
-            bondangle[anglenumber - 1][1].append(str(np.degrees(np.cos(angle3))))
-            atomb4 = np.array(bondcoords[i][0][1])
-            atoma4 = np.array(bondcoords[i][1][2][1])
-            atomc4 = np.array(bondcoords[i][1][3][1])
-            F4 = atoma4 - atomb4
-            E4 = atomb4 - atomc4
-            angle4 = np.dot(F4, E4)
-            bondangle.append([[bondcoords[i][1][2][0], bondcoords[i][0][0], bondcoords[i][1][3][0]], []])
-            anglenumber +=1
-            numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][2][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][3][0])])])
-            bondangle[anglenumber - 1][1].append(str(np.degrees(np.cos(angle4))))
-            atomb5 = np.array(bondcoords[i][0][1])
-            atoma5 = np.array(bondcoords[i][1][1][1])
-            atomc5 = np.array(bondcoords[i][1][3][1])
-            F5 = atoma5 - atomb5
-            E5 = atomb5 - atomc5
-            angle5 = np.dot(F5, E5)
-            bondangle.append([[bondcoords[i][1][1][0], bondcoords[i][0][0], bondcoords[i][1][3][0]], []])
-            anglenumber +=1
-            numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][1][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][3][0])])])
-            bondangle[anglenumber - 1][1].append(str(np.degrees(np.cos(angle5))))
+        numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][1][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][2][0])])])
+        bondangle[anglenumber - 1][1].append(str(angle1))
+        atomb2 = np.array(bondcoords[i][0][1])
+        atoma2 = np.array(bondcoords[i][1][0][1])
+        atomc2 = np.array(bondcoords[i][1][2][1])
+        F2 = atoma2 - atomb2
+        E2 = atomb2 - atomc2
+        dot2 = np.dot(F2, E2)
+        normF2 = np.linalg.norm(F2)
+        normE2 = np.linalg.norm(E2)
+        cosangle2 = dot2/(normE2 * normF2)
+        angle2 = 180 - np.degrees(np.arccos(cosangle2))
+        bondangle.append([[bondcoords[i][1][0][0], bondcoords[i][0][0], bondcoords[i][1][2][0]], []])
+        anglenumber +=1
+        numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][0][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][2][0])])])
+        bondangle[anglenumber - 1][1].append(str(angle2))
+    if len(bondcoords[i][1]) == 4:
+        atomb1 = np.array(bondcoords[i][0][1])
+        atoma1 = np.array(bondcoords[i][1][1][1])
+        atomc1 = np.array(bondcoords[i][1][2][1])
+        F1 = atoma1 - atomb1
+        E1 = atomb1 - atomc1
+        dot1 = np.dot(F1, E1)
+        normF1 = np.linalg.norm(F1)
+        normE1 = np.linalg.norm(E1)
+        cosangle1 = dot1/(normE1 * normF1)
+        angle1 = 180 - np.degrees(np.arccos(cosangle1))
+        bondangle.append([[bondcoords[i][1][1][0], bondcoords[i][0][0], bondcoords[i][1][2][0]], []])
+        anglenumber +=1
+        numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][1][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][2][0])])])
+        bondangle[anglenumber - 1][1].append(str(angle1))
+        atomb2 = np.array(bondcoords[i][0][1])
+        atoma2 = np.array(bondcoords[i][1][0][1])
+        atomc2 = np.array(bondcoords[i][1][2][1])
+        F2 = atoma2 - atomb2
+        E2 = atomb2 - atomc2
+        dot2 = np.dot(F2, E2)
+        normF2 = np.linalg.norm(F2)
+        normE2 = np.linalg.norm(E2)
+        cosangle2 = dot2/(normE2 * normF2)
+        angle2 = 180 - np.degrees(np.arccos(cosangle2))
+        bondangle.append([[bondcoords[i][1][0][0], bondcoords[i][0][0], bondcoords[i][1][2][0]], []])
+        anglenumber +=1
+        numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][0][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][2][0])])])
+        bondangle[anglenumber - 1][1].append(str(angle2))
+        atomb3 = np.array(bondcoords[i][0][1])
+        atoma3 = np.array(bondcoords[i][1][0][1])
+        atomc3 = np.array(bondcoords[i][1][3][1])
+        F3 = atoma3 - atomb3
+        E3 = atomb3 - atomc3
+        dot3 = np.dot(F3, E3)
+        normF3 = np.linalg.norm(F3)
+        normE3 = np.linalg.norm(E3)
+        cosangle3 = dot3/(normE3 * normF3)
+        angle3 = 180 - np.degrees(np.arccos(cosangle3))
+        bondangle.append([[bondcoords[i][1][0][0], bondcoords[i][0][0], bondcoords[i][1][3][0]], []])
+        anglenumber +=1
+        numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][0][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][3][0])])])
+        bondangle[anglenumber - 1][1].append(str(angle3))
+        atomb4 = np.array(bondcoords[i][0][1])
+        atoma4 = np.array(bondcoords[i][1][2][1])
+        atomc4 = np.array(bondcoords[i][1][3][1])
+        F4 = atoma4 - atomb4
+        E4 = atomb4 - atomc4
+        dot4 = np.dot(F4, E4)
+        normF4 = np.linalg.norm(F4)
+        normE4 = np.linalg.norm(E4)
+        cosangle4 = dot4/(normE4 * normF4)
+        angle4 = 180 - np.degrees(np.arccos(cosangle4))
+        bondangle.append([[bondcoords[i][1][2][0], bondcoords[i][0][0], bondcoords[i][1][3][0]], []])
+        anglenumber +=1
+        numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][2][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][3][0])])])
+        bondangle[anglenumber - 1][1].append(str(angle4))
+        atomb5 = np.array(bondcoords[i][0][1])
+        atoma5 = np.array(bondcoords[i][1][1][1])
+        atomc5 = np.array(bondcoords[i][1][3][1])
+        F5 = atoma5 - atomb5
+        E5 = atomb5 - atomc5
+        dot5 = np.dot(F5, E5)
+        normF5 = np.linalg.norm(F5)
+        normE5 = np.linalg.norm(E5)
+        cosangle5 = dot5/(normE5 * normF5)
+        angle5 = 180 - np.degrees(np.arccos(cosangle5))
+        bondangle.append([[bondcoords[i][1][1][0], bondcoords[i][0][0], bondcoords[i][1][3][0]], []])
+        anglenumber +=1
+        numberedangles.append([anglenumber, '-'.join([''.join(bondcoords[i][1][1][0]), ''.join(bondcoords[i][0][0]), ''.join(bondcoords[i][1][3][0])])])
+        bondangle[anglenumber - 1][1].append(str(angle5))
 for i in range (len(bondangle)):
+    #print('angle number {}, {}, with bond angle = {} degrees'.format(numberedangles[i][0], '-'.join(bondangle[i][0]), ', '.join(bondangle[i][1])), file = f3)
     print ('# {}  {}  {}'.format(numberedangles[i][0], '-'.join(bondangle[i][0]), ''.join(bondangle[i][1])), file = f3)
 f3.close()
 f4 = open('connectivity.txt', 'r')
@@ -203,6 +242,3 @@ f5.close()
 f5 = open('connectivity.txt', 'a')
 for i in range (len(angleidnumbers)):
     print (angleidnumbers[i], angleidnames[i], anglevalues[i], file = f5)
-print("All atom connectivity", file = f5)
-for i in range (len(allconnectivity)):
-    print (allconnectivity[i], file = f5)
