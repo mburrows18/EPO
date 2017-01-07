112d0c51d1ce664f4d000be7ee24457da3485a09
mburrows18 <mburrows@bu.edu>
2017-01-06 20:37:16 -0500

    epo.prm in progress

--- connectivitymap_clean.py
+++ connectivitymap_clean.py
@@ -1,20 +1,20 @@
-#//////////////////////////////////////
+
 from numpy import sqrt, power, linalg
 import numpy as np
-#//////////////////////////////////////
-#///////////////////////////////////
+
+
 f1 = open('EPO_optimized.pdb', 'r')
 #input file
 f2 = open('epo.itp', 'r')
 #input file
 f3 = open('connectivity.txt', 'w')
 #intermediate output file
-#///////////////////////////////////
-'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+'''
 atom number starts with 1
 atom index starts with 0, so atom number 1 has atom index 0
-'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-#///////////////////////////////////START VARIABLE LIST///////////////////////////////////
+'''
+
 itpList = f2.read().splitlines()
 #list of lines from itp input file, f2
 pdbList = f1.read().splitlines()
@@ -43,6 +43,8 @@ bondcoords = []
 		[[atom, coordinates], [[bonded atom, coordinates], etc]]
 		bondcoords[i][0] generates atom name with coordinates bondcoords[i][0][1]
 		bondcoords[i][1] generates [atom name, [coordinates]] of all bonded atoms'''
+bondcoordtype = []
+bondangletype = []
 bondlengths = []
 #all bond lengths with permutative duplicates in the form [atom1, atom2, bond length]
 bondangle = []
@@ -55,11 +57,17 @@ angleidnumbers = []
 angleidnames = []
 #stored list of angles in the form [atom1-central atom-atom2] with angle number 1 having index 0
 #extracted from connectivity.txt
+angleidtypes = []
+#stored list of angles in the form [atom1 type-central atom type-atom2 type] with angle number 1 having index 0
+#extracted from connectivity.txt
+angleIDtypes = []
 anglevalues = []
 #stored list of angle values with angle number 1 having index 0
 #extracted from connectivity.txt
 uniquebonds = []
 #unique bonds in the form [atom1, atom2]. index = bond number - 1
+uniquebondtypes = []
+#unique bond types in the form [atomtype1, atomtype2] index = bond number - 1
 uniquebondlengths = []
 #unique bond lengths without permutative duplicates in the form [atom1, atom2, bond length]
 bondtype = []
@@ -68,204 +76,223 @@ Aindex = 0
 #Aindex is NOT an ordered list -- intermediate variable used in section 2
 anglenumber = 0
 #anglenumber is NOT an ordered list -- intermediate variable used in section 3
-#///////////////////////////////////END VARIABLE LIST///////////////////////////////////
-#//////////////////////PRINTLIST FUNCTION//////////////////////
+
+
 def printlist(List):
 #prints all items in list, output file = f3 (connectivity.txt)
-  for i in range (len(List)):
-    print (List[i], file = f3)
-  return
-#//////////////////////PRINTLIST FUNCTION//////////////////////
-#/////////////AINDEX FUNCTION/////////////
+	for i in range (len(List)):
+		print (List[i], file = f3)
+	return
+
+
 def aindex(Aname):
 #extracts index from atom name
-  if Aname.startswith("C"):
-    Aindex = (int(Aname.strip("C")) - 1)
-  if Aname.startswith("H"):
-    Aindex = (int(Aname.strip("H")) - 1)
-  if Aname.startswith("N"):
-    Aindex = (int(Aname.strip("N")) - 1)
-  if Aname.startswith("O"):
-    Aindex = (int(Aname.strip("O")) - 1)
-  return Aindex
-#/////////////AINDEX FUNCTION/////////////
-#////////////////////////////CALCANGLE FUNCTION////////////////////////////
-def calcangle(bondangle, numberedangles, anglenumber, bondcoords, i, a, c):
+	if Aname.startswith("C"):
+		Aindex = (int(Aname.strip("C")) - 1)
+	if Aname.startswith("H"):
+		Aindex = (int(Aname.strip("H")) - 1)
+	if Aname.startswith("N"):
+		Aindex = (int(Aname.strip("N")) - 1)
+	if Aname.startswith("O"):
+		Aindex = (int(Aname.strip("O")) - 1)
+	return Aindex
+
+
+def calcangle(bondangle, numberedangles, anglenumber, bondcoords, bondcoordtype, i, a, c):
 #calculates bond angles
-  atomA = np.array(bondcoords[i][1][a][1])
-  atomB = np.array(bondcoords[i][0][1])
-  atomC = np.array(bondcoords[i][1][c][1])
-  AB = atomA - atomB
-  BC = atomB - atomC
-  dot = np.dot(AB, BC)
-  normAB = np.linalg.norm(AB)
-  normBC = np.linalg.norm(BC)
-  cosangle = dot/(normAB * normBC)
-  angle = 180 - np.degrees(np.arccos(cosangle))
-  anglenumber += 1
-  bondangle.append([[
-    bondcoords[i][1][a][0], 
-    bondcoords[i][0][0], 
-    bondcoords[i][1][c][0]], []])
-  numberedangles.append([anglenumber, '-'.join([
-    ''.join(bondcoords[i][1][a][0]), 
-    ''.join(bondcoords[i][0][0]), 
-    ''.join(bondcoords[i][1][c][0])])])
-  bondangle[anglenumber - 1][1].append(str(angle))
-  return anglenumber
-#////////////////////////////CALCANGLE FUNCTION////////////////////////////
-#/////////////////////////START SECTION ONE/////////////////////////
-'''''''''''''''''''''
+	atomA = np.array(bondcoords[i][1][a][1])
+	atomB = np.array(bondcoords[i][0][1])
+	atomC = np.array(bondcoords[i][1][c][1])
+	AB = atomA - atomB
+	BC = atomB - atomC
+	dot = np.dot(AB, BC)
+	normAB = np.linalg.norm(AB)
+	normBC = np.linalg.norm(BC)
+	cosangle = dot/(normAB * normBC)
+	angle = 180 - np.degrees(np.arccos(cosangle))
+	anglenumber += 1
+	bondangle.append([[
+		bondcoords[i][1][a][0], 
+		bondcoords[i][0][0], 
+		bondcoords[i][1][c][0]], []])
+	bondangletype.append([[
+		bondcoordtype[i][1][a][0], 
+		bondcoordtype[i][0][0], 
+		bondcoordtype[i][1][c][0]]])
+	numberedangles.append([anglenumber, '-'.join([
+		''.join(bondcoords[i][1][a][0]), 
+		''.join(bondcoords[i][0][0]), 
+		''.join(bondcoords[i][1][c][0])])])
+	bondangle[anglenumber - 1][1].append(str(angle))
+	return anglenumber
+
+
+'''
 extracts data from: 
-  itp file
-  pdb file
+	itp file
+	pdb file
 generates: 
-  coordinates[]
-  connectivity[]
+	coordinates[]
+	connectivity[]
 	uniqueconnectivity[]
-  bdist[]
+	bdist[]
 	uniquebdist[]
-  uniquebonds[]
-  atype[]
-  aname[]
-  atomcoords[]
-'''''''''''''''''''''
+	uniquebonds[]
+	atype[]
+	aname[]
+	atomcoords[]
+'''
 for line in itpList:
-  if line.startswith(' '):
-    words2 = line.split()
-    atype.append(words2[1])
+	if line.startswith(' '):
+		words2 = line.split()
+		atype.append(words2[1])
 for line in pdbList:
-  if line.startswith("HETATM"):
-    words = line.split()
-    coordinates.append((words[1], 
-      (float(words[5]), float(words[6]), float(words[7]))))
-    aname.append(words[2])
+	if line.startswith("HETATM"):
+		words = line.split()
+		coordinates.append((words[1], 
+			(float(words[5]), float(words[6]), float(words[7]))))
+		aname.append(words[2])
 for i in range(len(coordinates)):
-  atom_coord_x = coordinates[i][1][0]
-  atom_coord_y = coordinates[i][1][1]
-  atom_coord_z = coordinates[i][1][2]
-  connectivity.append([aname[i], []])
-  uniqueconnectivity.append([aname[i], []])
-  bdist.append([aname[i], [], []])
-  uniquebdist.append([aname[i], [], []])
-  atomcoords.append((aname[i], 
-    (atom_coord_x, atom_coord_y, atom_coord_z)))
-  for j in range(len(coordinates)):
-    target_coord_x = coordinates[j][1][0]
-    target_coord_y = coordinates[j][1][1]
-    target_coord_z = coordinates[j][1][2]
-    distance = sqrt(
-      power((target_coord_x - atom_coord_x), 2) +
-      power((target_coord_y - atom_coord_y), 2) +
-      power((target_coord_z - atom_coord_z), 2))
-    if (distance < dist_thresh) and (i != j) and (i < j):
-      uniquebonds.append([aname[i], aname[j]])
-      uniqueconnectivity[i][1].append(aname[j])
-      uniquebdist[i][1].append(aname[j])
-      uniquebdist[i][2].append(str(distance))
-    if (distance < dist_thresh) and (i != j):
-      connectivity[i][1].append(aname[j])
-      bdist[i][1].append(aname[j])
-      bdist[i][2].append(str(distance))
-#//////////////////////////END SECTION ONE//////////////////////////
-#//////////////////////////////START SECTION TWO//////////////////////////////
-'''''''''''''''''''''
+	atom_coord_x = coordinates[i][1][0]
+	atom_coord_y = coordinates[i][1][1]
+	atom_coord_z = coordinates[i][1][2]
+	connectivity.append([aname[i], []])
+	uniqueconnectivity.append([aname[i], []])
+	bdist.append([aname[i], [], []])
+	uniquebdist.append([aname[i], [], []])
+	atomcoords.append((aname[i], 
+		(atom_coord_x, atom_coord_y, atom_coord_z)))
+	for j in range(len(coordinates)):
+		target_coord_x = coordinates[j][1][0]
+		target_coord_y = coordinates[j][1][1]
+		target_coord_z = coordinates[j][1][2]
+		distance = sqrt(
+			power((target_coord_x - atom_coord_x), 2) +
+			power((target_coord_y - atom_coord_y), 2) +
+			power((target_coord_z - atom_coord_z), 2))
+		if (distance < dist_thresh) and (i != j) and (i < j):
+			uniquebonds.append([aname[i], aname[j]])
+			uniquebondtypes.append([atype[i], atype[j]])
+			uniqueconnectivity[i][1].append(aname[j])
+			uniquebdist[i][1].append(aname[j])
+			uniquebdist[i][2].append(str(distance))
+		if (distance < dist_thresh) and (i != j):
+			connectivity[i][1].append(aname[j])
+			bdist[i][1].append(aname[j])
+			bdist[i][2].append(str(distance))
+
+
+'''
 uses:
 	aindex(Aname)
 generates:
-  bondcoords[]
-  bondlengths[]
+	bondcoords[]
+	bondlengths[]
 	uniquebondlengths[]
-'''''''''''''''''''''
+'''
 for i in range(len(connectivity)):
-  if len(connectivity[i][1]) != 0:
-    bondcoords.append([[aname[i], atomcoords[i][1]], []])
-    for j in range(len(connectivity[i][1])):
-      Aname = connectivity[i][1][j]
-      Aindex = aindex(Aname)
-      bondlengths.append((aname[i], Aname, bdist[i][2][j]))
-      bondcoords[i][1].append([connectivity[i][1][j], atomcoords[Aindex][1]])
+	if len(connectivity[i][1]) != 0:
+		bondcoords.append([[aname[i], atomcoords[i][1]], []])
+		bondcoordtype.append([[atype[i], atomcoords[i][1]], []])
+		for j in range(len(connectivity[i][1])):
+			Aname = connectivity[i][1][j]
+			Aindex = aindex(Aname)
+			bondlengths.append((aname[i], Aname, bdist[i][2][j]))
+			bondcoords[i][1].append([connectivity[i][1][j], atomcoords[Aindex][1]])
+			bondcoordtype[i][1].append([atype[Aindex], atomcoords[Aindex][1]])
 for i in range(len(uniqueconnectivity)):
-  if len(uniqueconnectivity[i][1]) != 0:
-    bondcoords.append([[aname[i], atomcoords[i][1]], []])
-    for j in range(len(uniqueconnectivity[i][1])):
-      Aname = uniqueconnectivity[i][1][j]
-      Aindex = aindex(Aname)
-      uniquebondlengths.append((aname[i], Aname, uniquebdist[i][2][j]))
-#///////////////////////////////END SECTION TWO///////////////////////////////
-#///////////////////////////////START SECTION THREE///////////////////////////////
-'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+	if len(uniqueconnectivity[i][1]) != 0:
+		bondcoords.append([[aname[i], atomcoords[i][1]], []])
+		for j in range(len(uniqueconnectivity[i][1])):
+			Aname = uniqueconnectivity[i][1][j]
+			Aindex = aindex(Aname)
+			uniquebondlengths.append((aname[i], Aname, uniquebdist[i][2][j]))
+
+
+'''
 uses:
 	calcangle(bondangle, numberedangles, anglenumber, bondcoords, i, a, c)
 generates:
 	bondangle[]
 	numberedangles[]
-'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+'''
 for i in range(len(bondcoords)):
-  if len(bondcoords[i][1]) == 2:
-    anglenumber = calcangle(
-      bondangle, 
-      numberedangles, 
-      anglenumber, 
-      bondcoords, i, 0, 1)
-  if len(bondcoords[i][1]) == 3:
-    anglenumber = calcangle(
-      bondangle, 
-      numberedangles, 
-      anglenumber, 
-      bondcoords, i, 0, 1)
-    anglenumber = calcangle(
-      bondangle, 
-      numberedangles, 
-      anglenumber, 
-      bondcoords, i, 1, 2)
-    anglenumber = calcangle(
-      bondangle, 
-      numberedangles, 
-      anglenumber, 
-      bondcoords, i, 0, 2)
-  if len(bondcoords[i][1]) == 4:
-    anglenumber = calcangle(
-      bondangle, 
-      numberedangles, 
-      anglenumber, 
-      bondcoords, i, 0, 1)
-    anglenumber = calcangle(
-      bondangle, 
-      numberedangles, 
-      anglenumber, 
-      bondcoords, i, 1, 2)
-    anglenumber = calcangle(
-      bondangle, 
-      numberedangles, 
-      anglenumber, 
-      bondcoords, i,  0, 2)
-    anglenumber = calcangle(
-      bondangle, 
-      numberedangles, 
-      anglenumber, 
-      bondcoords, i, 0, 3)
-    anglenumber = calcangle(
-      bondangle, 
-      numberedangles, 
-      anglenumber, 
-      bondcoords, i, 2, 3)
-    anglenumber = calcangle(
-      bondangle, 
-      numberedangles, 
-      anglenumber, 
-      bondcoords, i, 1, 3) 
+	if len(bondcoords[i][1]) == 2:
+		anglenumber = calcangle(
+			bondangle, 
+			numberedangles, 
+			anglenumber, 
+			bondcoords,
+			bondcoordtype, i, 0, 1)
+	if len(bondcoords[i][1]) == 3:
+		anglenumber = calcangle(
+			bondangle, 
+			numberedangles, 
+			anglenumber, 
+			bondcoords, 
+			bondcoordtype, i, 0, 1)
+		anglenumber = calcangle(
+			bondangle, 
+			numberedangles, 
+			anglenumber, 
+			bondcoords, 
+			bondcoordtype, i, 1, 2)
+		anglenumber = calcangle(
+			bondangle, 
+			numberedangles, 
+			anglenumber, 
+			bondcoords, 
+			bondcoordtype, i, 0, 2)
+	if len(bondcoords[i][1]) == 4:
+		anglenumber = calcangle(
+			bondangle, 
+			numberedangles, 
+			anglenumber, 
+			bondcoords, 
+			bondcoordtype, i, 0, 1)
+		anglenumber = calcangle(
+			bondangle, 
+			numberedangles, 
+			anglenumber, 
+			bondcoords, 
+			bondcoordtype, i, 1, 2)
+		anglenumber = calcangle(
+			bondangle, 
+			numberedangles, 
+			anglenumber, 
+			bondcoords, 
+			bondcoordtype, i,	0, 2)
+		anglenumber = calcangle(
+			bondangle, 
+			numberedangles, 
+			anglenumber, 
+			bondcoords, 
+			bondcoordtype, i, 0, 3)
+		anglenumber = calcangle(
+			bondangle, 
+			numberedangles, 
+			anglenumber, 
+			bondcoords, 
+			bondcoordtype, i, 2, 3)
+		anglenumber = calcangle(
+			bondangle, 
+			numberedangles, 
+			anglenumber, 
+			bondcoords, 
+			bondcoordtype, i, 1, 3) 
 for i in range(len(bondangle)):
-  print ('# {}  {}  {}'.format(
-    numberedangles[i][0], 
-    '-'.join(bondangle[i][0]), 
-    ''.join(bondangle[i][1])), 
-    file = f3)
-#////////////////////////////////END SECTION THREE////////////////////////////////
-#////////////////////START SECTION FOUR////////////////////
+	print ('# {}	{}	{}'.format(
+		numberedangles[i][0], 
+		'-'.join(bondangle[i][0]), 
+		''.join(bondangle[i][1])), 
+		file = f3)
+	print ('2# {}	{}'.format(
+		numberedangles[i][0], 
+		'-'.join(bondangletype[i][0])), 
+		file = f3)
 print ("unique bonds", file = f3)
 for i in range (len(uniquebonds)):
-  print (i+1,uniquebonds[i], file = f3)
+	print (i+1,uniquebonds[i], file = f3)
 print ("unique bond lengths", file = f3)
 for i in range(len(uniquebondlengths)):
 	print (i+1,uniquebondlengths[i], file = f3)
@@ -273,7 +300,7 @@ print ("atom types", file = f3)
 for i in range(len(aname)):
 	print ('{} -- {}'.format(aname[i], atype[i]), file = f3)
 f3.close()
-#/////////////////////END SECTION FOUR/////////////////////
+
 f4 = open('connectivity.txt', 'r')
 connectivityList = f4.read().splitlines()
 for line in connectivityList:
@@ -281,14 +308,35 @@ for line in connectivityList:
     words3 = line.split()
     angleidnumbers.append(words3[1])
     angleidnames.append(words3[2])
-    anglevalues.append(words3[3])    
+    anglevalues.append(words3[3])
+  if line.startswith("2#"):
+    words4 = line.split()
+    angleidtypes.append(words4[2])    
 f4.close()
 f5 = open('connectivity.txt', 'w')
 for line in connectivityList:
-  if not line.startswith("#"):
+  if not line.startswith("#") and not line.startswith("2#"):
     print(line, file = f5)
 f5.close()
 f5 = open('connectivity.txt', 'a')
 print ("angles", file = f5)
 for i in range (len(angleidnumbers)):
-  print (angleidnumbers[i], angleidnames[i], anglevalues[i], file = f5)
+	print (angleidnumbers[i], angleidnames[i], angleidtypes[i], anglevalues[i], file = f5)
+f5.close()
+
+
+f6 = open('EPO.prm', 'w')
+print ("\n[ bondtypes ]\n;     i           j   func                    b0                    kb", file = f6)
+for i in range(len(uniquebonds)):
+  print("     {}          {}       1         {}        ".format(
+		uniquebondtypes[i][0], 
+		uniquebondtypes[i][1], 
+		uniquebondlengths[i][2]), 
+		file = f6)
+print ("\n\n", file = f6)
+print ("[ angletypes ]\n;     i         j        k    func      theta0       ktheta          ub0          kub", file = f6)
+for i in range(len(angleidtypes)):
+	line = str(angleidtypes[i])
+	angleIDtypes.append(line.split("-"))
+for i in range(len(angleidtypes)):
+	print ("    {}       {}       {}".format(angleIDtypes[i][0], angleIDtypes[i][1], angleIDtypes[i][2]), file = f6)
